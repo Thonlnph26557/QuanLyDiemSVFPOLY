67a2cb0f98b201d687d87c71eed14ceea5eebfa2
FNU LNU <thao@phuong203.onmicrosoft.com>
2022-11-14 21:59:15 +0700

    CodeFirst9Table

---
+++ QLDSVFPOLY.DTO/Entities/DiemSo.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QLDSVFPOLY.DTO.Entities
+{
+    public class DiemSo
+    {
+        public DiemSo()
+        {
+        }
+
+        public DiemSo(Guid idChiTietLopHoc, Guid idSinhVien, string tenDiemSo, double trongSo, double diem, DateTime ngayTao, int trangThai)
+        {
+            IdChiTietLopHoc = idChiTietLopHoc;
+            IdSinhVien = idSinhVien;
+            TenDiemSo = tenDiemSo;
+            TrongSo = trongSo;
+            Diem = diem;
+            NgayTao = ngayTao;
+            TrangThai = trangThai;
+        }
+
+        public Guid IdChiTietLopHoc { get; set; }
+        public Guid IdSinhVien { get; set; }
+        public string TenDiemSo { get; set; }
+        public double TrongSo { get; set; }
+        public double Diem { get; set; }
+        public DateTime NgayTao { get; set; }
+        public int TrangThai { get; set; }
+
+
+        public SinhVien SinhVien { get; set; }
+        public ChiTietLopHoc ChiTietLopHoc { get; set; }
+    }
+}
---
+++ QLDSVFPOLY.DTO/Entities/KiHoc.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QLDSVFPOLY.DTO.Entities
+{
+    public class KiHoc
+    {
+
+        public KiHoc()
+        {
+
+        }
+
+        public KiHoc(Guid id, string ten, int namHoc, DateTime ngayBatDau, DateTime ngayKetThuc, DateTime ngayTao, int trangThai)
+        {
+            Id = id;
+            Ten = ten;
+            NamHoc = namHoc;
+            NgayBatDau = ngayBatDau;
+            NgayKetThuc = ngayKetThuc;
+            NgayTao = ngayTao;
+            TrangThai = trangThai;
+        }
+
+        public Guid Id { get; set; }
+        public string Ten { get; set; }
+        public int NamHoc { get; set; }
+        public DateTime NgayBatDau { get; set; }
+        public DateTime NgayKetThuc { get; set; }
+        public DateTime NgayTao { get; set; }
+        public int TrangThai { get; set; }
+
+
+
+        // relationship 
+
+        public List<ChiTietLopHoc> ChiTietLopHocs { get; set; }
+    }
+}
