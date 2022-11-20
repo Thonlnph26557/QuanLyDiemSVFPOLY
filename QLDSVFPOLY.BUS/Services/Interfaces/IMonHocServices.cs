061544ef08a6278802e5d669be6a34fbc6d8e4d8
Sonnhph27363 <sonnhph27363@fpt.edu.vn>
2022-11-20 15:10:06 +0700

    CRUD Môn Học + CRUD Sinh Viên + Seeding Data

---
+++ QLDSVFPOLY.BUS/Services/Interfaces/IMonHocServices.cs
@@ -0,0 +1,19 @@
+﻿using QLDSVFPOLY.BUS.ViewModels.MonHoc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QLDSVFPOLY.BUS.Services.Interfaces
+{
+    public interface IMonHocServices
+    {
+        Task<List<MonHocVM>> GetAllAsync(MonHocSearchVM obj);
+        Task<List<MonHocVM>> GetAllActiveAsync(MonHocSearchVM obj);
+        Task<MonHocVM> GetByIdAsync(Guid id);
+        Task<bool> CreateAsync(MonHocCreateVM obj);
+        Task<bool> UpdateAsync(Guid id,MonHocUpdateVM obj);
+        Task<bool> RemoveAsync(Guid id);
+    }
+}
---
+++ QLDSVFPOLY.BUS/ViewModels/MonHoc/MonHocSearchVM.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QLDSVFPOLY.BUS.ViewModels.MonHoc
+{
+    public class MonHocSearchVM
+    {
+        public Guid Id { get; set; }
+        public string Ma { get; set; }
+        public string Ten { get; set; }
+        public string DuongDanAnh { get; set; }
+        public DateTime NgayTao { get; set; }
+        public int TrangThai { get; set; }
+        public Guid IdChuyenNganh { get; set; }
+
+    }
+}
