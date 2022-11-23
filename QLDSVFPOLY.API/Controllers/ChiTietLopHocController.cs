ffe2304aa1c4d3b9a1d8fa8a886bdb016203a7f2
Sonnhph27363 <sonnhph27363@fpt.edu.vn>
2022-11-23 15:07:39 +0700

    Sonnhph27363_Controller(KiHoc+ChiTietLopHoc)

---
+++ QLDSVFPOLY.API/Controllers/ChiTietLopHocController.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using QLDSVFPOLY.BUS.Services.Interfaces;
+using QLDSVFPOLY.BUS.ViewModels.ChiTietLopHoc;
+
+namespace QLDSVFPOLY.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ChiTietLopHocController : ControllerBase
+    {
+        private readonly IChiTietLopHocServices _iChiTietLopHocServices;
+
+        public ChiTietLopHocController(IChiTietLopHocServices iChiTietLopHocServices)
+        {
+            _iChiTietLopHocServices = iChiTietLopHocServices;
+        }
+
+        [HttpGet("all")]
+        public async Task<IActionResult> GetAllChiTietLopHoc()
+        {
+            var listChiTietLopHoc = await _iChiTietLopHocServices.GetAllAsyncChiTietLopHoc(null);
+
+            return Ok(listChiTietLopHoc);
+        }
+
+        //GetById ??? 
+        [HttpGet]
+        [Route("{id}")]
+        public async Task<IActionResult> GetChiTietLopHocById(Guid id)
+        {
+            var chiTietLopHoc = await _iChiTietLopHocServices.GetByIdAsyncChiTietLopHoc(id);
+            return Ok(chiTietLopHoc);
+        }
+
+        //Controller dc gọi khi thêm obj
+        [HttpPost]
+        public async Task<IActionResult> CreateChiTietLopHoc([FromBody] ChiTietLopHocCreateViewmodel chiTietLopHoc)
+        {
+            if (chiTietLopHoc == null)
+                return BadRequest();
+
+            var newChiTietLopHoc = await _iChiTietLopHocServices.CreateAsync(chiTietLopHoc);
+
+            return Ok(newChiTietLopHoc);
+        }
+
+        //Controller dc gọi khi update obj
+        [HttpPut]
+        [Route("{id}")]
+        public async Task<IActionResult> UpdateChiTietLopHoc(Guid id, [FromBody] ChiTietLopHocUpdateViewmodel chiTietLopHoc)
+        {
+            var result = await _iChiTietLopHocServices.UpdateAsync(id, chiTietLopHoc);
+            return Ok(result);
+        }
+
+        [HttpDelete]
+        [Route("{id}")]
+        public async Task<IActionResult> DeleteChiTietLopHoc(Guid id)
+        {
+            var result = await _iChiTietLopHocServices.RemoveAsync(id);
+            return Ok(result);
+        }
+    }
+}
---
+++ QLDSVFPOLY.API/Controllers/KiHocController.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using QLDSVFPOLY.BUS.Services.Interfaces;
+using QLDSVFPOLY.BUS.ViewModels.KiHoc;
+
+namespace QLDSVFPOLY.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class KiHocController : ControllerBase
+    {
+        private readonly IKiHocServices _iKiHocServices;
+        public KiHocController(IKiHocServices iKiHocServices)
+        {
+            _iKiHocServices = iKiHocServices;
+        }
+
+        [HttpGet("all")]
+        public async Task<IActionResult> GetAllKiHoc()
+        {
+            var listKiHoc = await _iKiHocServices.GetAllAsyncKiHoc(null);
+            return Ok(listKiHoc);
+        }
+
+        [HttpGet("allActive")]
+        public async Task<IActionResult> GetAllActiveKiHoc()
+        {
+            var listKiHoc = await _iKiHocServices.GetAllActiveAsyncKiHoc(null);
+            return Ok(listKiHoc);
+        }
+
+
+
+
+        //GetById
+        [HttpGet]
+        [Route("{id}")]
+        public async Task<IActionResult> GetKiHocById(Guid id)
+        {
+            var kiHoc = await _iKiHocServices.GetByIdAsyncKiHoc(id);
+            return Ok(kiHoc);   
+        }
+
+        //Controller đc gọi khi thêm obj
+        [HttpPost]
+        public async Task<IActionResult> CreateKiHoc([FromBody] KiHocCreateViewmodel kiHoc)
+        {
+            if (kiHoc == null) return BadRequest();
+            var newKiHoc = await _iKiHocServices.CreateAsync(kiHoc);
+            return Ok(newKiHoc);
+        }
+
+        //Controller đc gọi khi update obj
+        [HttpPut]
+        [Route("{id}")]
+        public async Task<IActionResult> UpdateKiHoc(Guid id, [FromBody] KiHocUpdateViewmodel kiHoc)
+        {
+            var result = await _iKiHocServices.UpdateAsync(id, kiHoc);
+            return Ok(result);
+        }
+
+        [HttpDelete]
+        [Route("{id}")]
+        public async Task<IActionResult> DeleteKiHoc(Guid id)
+        {
+            var result = await _iKiHocServices.RemoveAsync(id);
+            return Ok(result);
+        }
+    }
+}
