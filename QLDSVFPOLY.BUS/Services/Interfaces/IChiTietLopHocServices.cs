85c8c28d1c37d075bf4be42da596b5341e78aafe
LINH TRAN <linhtdph27103@fpt.edu.vn>
2022-12-04 22:54:10 +0700

    linhtd-ph27103 - Ser+Repo+View(CTLH,KH)

---
+++ QLDSVFPOLY.BUS/Services/Interfaces/IChiTietLopHocServices.cs
@@ -0,0 +1,23 @@
+﻿using QLDSVFPOLY.BUS.ViewModels.ChiTietLopHoc;
+using QLDSVFPOLY.BUS.ViewModels.LopHoc;
+using QLDSVFPOLY.DAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QLDSVFPOLY.BUS.Services.Interfaces
+{
+    public interface IChiTietLopHocServices
+    {
+
+        Task<List<ChiTietLopHocViewmodel>> GetAllAsync(ChiTietLopHocSearchViewmodel obj);
+        Task<List<ChiTietLopHocViewmodel>> GetAllActiveAsync(ChiTietLopHocSearchViewmodel obj);
+        Task<ChiTietLopHocViewmodel> GetByIdAsync(Guid id);
+
+        Task<bool> CreateAsync(ChiTietLopHocCreateViewmodel obj);
+        Task<bool> UpdateAsync(Guid id, ChiTietLopHocUpdateViewmodel obj);
+        Task<bool> RemoveAsync(Guid id);
+    }
+}
---
+++ QLDSVFPOLY.BUS/ViewModels/ChiTietLopHoc/ChiTietLopHocViewmodel.cs
@@ -0,0 +1,24 @@
+﻿using QLDSVFPOLY.DAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Đổi tên namespace đúng model
+namespace QLDSVFPOLY.BUS.ViewModels.ChiTietLopHoc
+{
+    //> public
+    public class ChiTietLopHocViewmodel
+    {
+        public Guid Id { get; set; }
+        public Guid IdLopHoc { get; set; }
+        public Guid IdMonHoc { get; set; }
+        public Guid IdGiangVien { get; set; }
+        public Guid IdKiHoc { get; set; }
+        public int SoLuongSinhVien { get; set; }
+        public DateTime NgayTao { get; set; }
+        public int TrangThai { get; set; }
+
+    }
+}
